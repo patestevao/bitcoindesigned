a0b9103f8cdff11ac7ad40659851720faf5cfa3e
marcoagner <marco@agner.io>
2018-11-14 18:27:12 +0000

    creates feature for adding infographic sources to the page

--- bitcoin_designed/public/admin.py
+++ bitcoin_designed/public/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Infographic, Language, InfographicURL, Tag
+from .models import Infographic, Language, InfographicURL, Tag, InfographicSource
 
 
 # Register your models here.
@@ -9,9 +9,14 @@ class InfographicURLAdminInline(admin.TabularInline):
     extra = 0
 
 
+class InfographicSourceAdminInline(admin.TabularInline):
+    model = InfographicSource
+    extra = 0
+
+
 @admin.register(Infographic)
 class InfographicAdmin(admin.ModelAdmin):
-    inlines = (InfographicURLAdminInline,)
+    inlines = (InfographicURLAdminInline, InfographicSourceAdminInline)
     pass
 
 
@@ -22,6 +27,11 @@ class LanguageAdmin(admin.ModelAdmin):
 
 @admin.register(InfographicURL)
 class InfographicURLAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(InfographicSource)
+class InfographicSourceAdmin(admin.ModelAdmin):
     pass
 
 
